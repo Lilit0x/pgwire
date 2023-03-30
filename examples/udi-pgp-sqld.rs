48336ba9f059024b93836c6716a4e3a823e0f03a
Lilit0x <baasisek01@gmail.com>
2023-03-30 18:09:49 +0100

    chore: formatted response

--- examples/udi-pgp-sqld.rs
+++ examples/udi-pgp-sqld.rs
@@ -1,14 +1,15 @@
-use std::io::{self, Write};
 use std::process::Command;
+use std::str;
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use futures::{stream, StreamExt};
 use tokio::net::TcpListener;
 
 use pgwire::api::auth::noop::NoopStartupHandler;
 use pgwire::api::query::{PlaceholderExtendedQueryHandler, SimpleQueryHandler};
-use pgwire::api::results::{Response, Tag};
-use pgwire::api::{ClientInfo, MakeHandler, StatelessMakeHandler};
+use pgwire::api::results::{DataRowEncoder, FieldFormat, FieldInfo, Response, QueryResponse};
+use pgwire::api::{ClientInfo, MakeHandler, StatelessMakeHandler, Type};
 use pgwire::error::PgWireResult;
 use pgwire::tokio::process_socket;
 
@@ -20,18 +21,67 @@ impl SimpleQueryHandler for PGPProcessor {
     where
         C: ClientInfo + Unpin + Send + Sync,
     {
-        println!("{:?}", query);
         let output = Command::new("osqueryi")
-            .arg("SELECT name, path, pid FROM processes;")
+            .arg(query)
             .output()
-            .expect("failed to execute process");
+            .expect("failed to execute osquery query");
 
-        io::stdout().write_all(&output.stdout).unwrap();
-        io::stderr().write_all(&output.stderr).unwrap();
-        Ok(vec![Response::Execution(Tag::new_for_execution(
-            "OK",
-            Some(1),
+        let mut osquery_res = String::new();
+        osquery_res.push_str(match str::from_utf8(&output.stdout) {
+            Ok(val) => val,
+            Err(_) => panic!("got non UTF-8 data from output"),
+        });
+        let raw_res: Vec<&str> = osquery_res.lines().collect();
+
+        //format data to suit QueryResponse
+        let mut field_infos: Vec<FieldInfo> = vec![];
+        let mut data: Vec<(Option<i32>, Option<&str>)> = vec![];
+
+        for n in 0..raw_res.len() {
+            let text = raw_res[n];
+            if text.starts_with('|') {
+                //fields
+                if n == 1 {
+                    for field in text.split('|').collect::<Vec<&str>>().into_iter() {
+                        field_infos.push(FieldInfo::new(
+                            field.to_string(),
+                            None,
+                            None,
+                            Type::VARCHAR,
+                            FieldFormat::Text,
+                        ))
+                    }
+                } else {
+                    let mut count: i32 = 0;
+                    for row in text.split('|').collect::<Vec<&str>>().into_iter() {
+                        data.push((Some(count), Some(row)));
+                        count += 1;
+                    }
+                }
+            }
+        }
+
+        let schema = Arc::new(field_infos);
+        let schema_ref = schema.clone();
+        let data_row_stream = stream::iter(data.into_iter()).map(move |r| {
+            let mut encoder = DataRowEncoder::new(schema_ref.clone());
+            encoder.encode_field(&r.0)?;
+            encoder.encode_field(&r.1)?;
+
+            encoder.finish()
+        });
+        // io::stdout().write_all(&output.stdout).unwrap();
+        // io::stderr().write_all(&output.stderr).unwrap();
+
+        Ok(vec![Response::Query(QueryResponse::new(
+            schema,
+            data_row_stream,
         ))])
+
+        // Ok(vec![Response::Execution(Tag::new_for_execution(
+        //     "OK",
+        //     Some(1),
+        // ))])
     }
 }
 #[tokio::main]
