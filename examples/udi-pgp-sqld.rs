ba2d1ef7c6b0e2c315576b0ddc0ec0f7b4e6b118
Lilit0x <baasisek01@gmail.com>
2023-03-30 15:48:49 +0100

    chore: printing out basic messages

---
+++ examples/udi-pgp-sqld.rs
@@ -0,0 +1,56 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use tokio::net::TcpListener;
+
+use pgwire::api::auth::noop::NoopStartupHandler;
+use pgwire::api::query::{PlaceholderExtendedQueryHandler, SimpleQueryHandler};
+use pgwire::api::results::{Response, Tag};
+use pgwire::api::{ClientInfo, MakeHandler, StatelessMakeHandler};
+use pgwire::error::PgWireResult;
+use pgwire::tokio::process_socket;
+
+pub struct PGPProcessor;
+
+#[async_trait]
+impl SimpleQueryHandler for PGPProcessor {
+    async fn do_query<'a, C>(&self, _client: &C, query: &'a str) -> PgWireResult<Vec<Response<'a>>>
+    where
+        C: ClientInfo + Unpin + Send + Sync,
+    {
+        println!("{:?}", query);
+        Ok(vec![Response::Execution(Tag::new_for_execution(
+            "OK",
+            Some(1),
+        ))])
+    }
+}
+#[tokio::main]
+pub async fn main() {
+        let processor = Arc::new(StatelessMakeHandler::new(Arc::new(PGPProcessor)));
+    // We have not implemented extended query in this server, use placeholder instead
+    let placeholder = Arc::new(StatelessMakeHandler::new(Arc::new(
+        PlaceholderExtendedQueryHandler,
+    )));
+    let authenticator = Arc::new(StatelessMakeHandler::new(Arc::new(NoopStartupHandler)));
+
+    let server_addr = "127.0.0.1:5432";
+    let listener = TcpListener::bind(server_addr).await.unwrap();
+    println!("Listening to {}", server_addr);
+    loop {
+        let incoming_socket = listener.accept().await.unwrap();
+        let authenticator_ref = authenticator.make();
+        let processor_ref = processor.make();
+        let placeholder_ref = placeholder.make();
+        tokio::spawn(async move {
+            process_socket(
+                incoming_socket.0,
+                None,
+                authenticator_ref,
+                processor_ref,
+                placeholder_ref,
+            )
+            .await
+        });
+    }
+}
